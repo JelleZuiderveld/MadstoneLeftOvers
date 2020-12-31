138e8e7574cfc2bc7c66d8071ce425d99acee12c
Jelle <zuiderveldjelle@gmail.com>
2020-12-31 15:15:22 +0100

    Google APi working

---
+++ app/src/main/java/com/example/madstone/ui/MapFragment.kt
@@ -0,0 +1,81 @@
+package com.example.madstone.ui
+
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
+import android.location.Location
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import com.example.madstone.R
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import kotlinx.android.synthetic.main.fragment_map.*
+import java.util.jar.Manifest
+
+class MapFragment : Fragment(), OnMapReadyCallback {
+
+    private lateinit var currentLocation: Location
+    private lateinit var fusedLocation: FusedLocationProviderClient
+    private val permissionCode = 1
+    private lateinit var googleMap: GoogleMap
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        map_view.onCreate(savedInstanceState)
+        map_view.onResume()
+        map_view.getMapAsync(this)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_map, container, false)
+    }
+
+    private fun getAccces(){
+        if(ContextCompat.checkSelfPermission(requireActivity(),
+                android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+            googleMap.isMyLocationEnabled = true
+        }else{
+            ActivityCompat.requestPermissions(requireActivity(), arrayOf(
+                android.Manifest.permission.ACCESS_FINE_LOCATION), permissionCode)
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray
+    ) {
+        if(requestCode == permissionCode){
+            if(grantResults.contains(PackageManager.PERMISSION_GRANTED)){
+                googleMap.isMyLocationEnabled = true
+            }
+            else{
+                Toast.makeText(requireActivity(), "User denied permission", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+
+    override fun onMapReady(map: GoogleMap?) {
+        map?.let {
+            googleMap = it
+            getAccces()
+        }
+    }
+
+}
